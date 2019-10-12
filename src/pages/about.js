33141eeaf19dc4658dea18fd26a2d715687c96b9
Tyler McSilva <tylerbmcsilva@gmail.com>
2019-10-12 19:52:41 -0400

    file cleanup

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,18 @@
-import React  from "react"
-import Layout from "../components/layout"
-import SEO    from "../components/seo"
+import React  from 'react';
+import Layout from '../components/layout';
+import SEO    from '../components/seo';
+
 
 export default function AboutPage() {
   return (
     <Layout>
-    <SEO title="About" />
-    <section className="card">
-    <h1>Thanks for reading!</h1>
-      <p>The impeachment inquiry into Donald J. Trump seems to have happened fairly quickly, but by compiling into a linear timeline, you can see how the events have transpired over many months.</p>
-      <p>The goal of this timeline is to provide information in chronological order along with resources for people to learn more on their own.</p>
-      <p>If you would like to contribute, please contact me at <a href="mailto:info@impeachment.dev">info@impeachment.dev</a>.</p>
-    </section>
-  </Layout>
+      <SEO title="About" />
+      <section className="card">
+      <h1>Thanks for reading!</h1>
+        <p>The impeachment inquiry into Donald J. Trump seems to have happened fairly quickly, but by compiling into a linear timeline, you can see how the events have transpired over many months.</p>
+        <p>The goal of this timeline is to provide information in chronological order along with resources for people to learn more on their own.</p>
+        <p>If you would like to contribute, please contact me at <a href="mailto:info@impeachment.dev">info@impeachment.dev</a>.</p>
+      </section>
+    </Layout>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,13 @@
-import React        from "react"
-import { useState } from 'react';
+import Layout           from '../components/layout'
+import SEO              from '../components/seo'
+import React            from 'react';
+import rehypeReact      from 'rehype-react';
+import Tweet            from '../components/tweet';
+import { graphql }      from 'gatsby';
+import { Timeline }     from 'vertical-timeline-component-for-react';
+import { TimelineItem } from 'vertical-timeline-component-for-react';
+import { useState }     from 'react';
 
-import { StaticQuery, graphql } from 'gatsby';
-
-import Layout from "../components/layout"
-import SEO    from "../components/seo"
-import Tweet  from '../components/tweet';
-
-import { Timeline, TimelineItem }  from 'vertical-timeline-component-for-react';
-
-import rehypeReact from "rehype-react";
 
 const render = new rehypeReact({
   createElement: React.createElement,
@@ -18,7 +16,44 @@ const render = new rehypeReact({
   }
 }).Compiler;
 
-const dataQuery = graphql`
+
+export default function IndexPage({ data }) {
+  const { allMarkdownRemark } = data;
+  const { edges }             = allMarkdownRemark;
+  const [ dates, setDates ]   = useState(edges);
+
+  function handleToggle() {
+    const reversed = dates.reverse();
+    setDates([ ...reversed ]);
+  }
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <p style={{ fontStyle: 'italic', textAlign: 'center' }}>
+        ** This site is still being set-up and updated **
+      </p>
+      <button type="button" onClick={handleToggle}>Toggle Order Of Events</button>
+      <Timeline lineColor={'#ddd'} animate={false}>
+        {dates.map(({ node }) => {
+          return (
+            <TimelineItem
+              key={node.id}
+              id={node.frontmatter.date}
+              dateText={node.frontmatter.title}
+              dateInnerStyle={{ background: '#B71C1C', color: '#FFF' }}
+              style={{ color: '#B71C1C' }}>
+              {render(node.htmlAst)}
+            </TimelineItem>
+          );
+        })}
+      </Timeline>
+    </Layout>
+  );
+}
+
+
+export const query = graphql`
   query {
     allMarkdownRemark(sort: {fields: frontmatter___date, order: ASC}) {
       edges {
@@ -36,52 +71,3 @@ const dataQuery = graphql`
     }
   }
 `;
-
-
-const IndexPage = function({ data }) {
-  const { allMarkdownRemark } = data;
-  const { edges }             = allMarkdownRemark;
-  const [ dates, setDates ]   = useState(edges);
-
-  function handleToggle() {
-    const reversed = dates.reverse();
-    setDates([ ...reversed ]);
-  }
-
-  return (
-    <Layout>
-    <SEO title="Home" />
-    <p style={{ fontStyle: 'italic', textAlign: 'center' }}>** This site is still being set-up and updated **</p>
-    <button type="button" onClick={handleToggle}>Toggle Order Of Events</button>
-    <DateTimeline dates={dates}/>
-  </Layout>
-  );
-}
-
-
-function DateTimeline({ dates }) {
-  return (
-    <Timeline lineColor={'#ddd'} animate={false}>
-      {dates.map(({ node }) => {
-        return (
-          <TimelineItem
-            key={node.id}
-            id={node.frontmatter.date}
-            dateText={node.frontmatter.title}
-            dateInnerStyle={{ background: '#B71C1C', color: '#FFF' }}
-            style={{ color: '#B71C1C' }}>
-            {render(node.htmlAst)}
-          </TimelineItem>
-        );
-      })}
-    </Timeline>
-  );
-}
-
-
-export default props => (
-  <StaticQuery
-    query={dataQuery}
-    render={data => <IndexPage data={data} {...props} />}
-  />
-);
