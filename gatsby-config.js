97bd3f1c2a6777004dd65fcc96c5a27ce8765727
Tyler McSilva <tylerbmcsilva@gmail.com>
2019-10-12 20:51:10 -0400

    Fix build :grimacing:

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'data',
-        path: '${__dirname}/src/data',
+        path: `${__dirname}/src/data`,
       },
     },
     'gatsby-transformer-remark',
