3def958dd5ad2853abc1e47e07d4b11581cad3cf
Tyler McSilva <tylerbmcsilva@gmail.com>
2019-10-12 20:39:25 -0400

    Small updates

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,7 +11,7 @@ export default function AboutPage() {
       <h1>Thanks for reading!</h1>
         <p>The impeachment inquiry into Donald J. Trump seems to have happened fairly quickly, but by compiling into a linear timeline, you can see how the events have transpired over many months.</p>
         <p>The goal of this timeline is to provide information in chronological order along with resources for people to learn more on their own.</p>
-        <p>If you would like to contribute, please contact me at <a href="mailto:info@impeachment.dev">info@impeachment.dev</a>.</p>
+        <p style={{ marginBottom: 0 }}>If you would like to contribute, please contact me at <a href="mailto:info@impeachment.dev">info@impeachment.dev</a>.</p>
       </section>
     </Layout>
   );
